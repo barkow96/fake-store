f49c3767f884ce07c4df51d7587e04f7a1ca495f
Bartosz Kowal <bartoszkowal96@gmail.com>
2026-02-07 17:37:21 +0100

    feat(services): Improve error handling in getProducts service

--- services/product.service.ts
+++ services/product.service.ts
@@ -1,16 +1,28 @@
 import { OApiEndpointUrl } from "@/constants";
 import { apiToProductsList } from "@/mappers";
 import type { Product } from "@/types";
+import { logError } from "@/utils";
 
 export const ProductService = {
   async getProducts(): Promise<Product[]> {
-    const response = await fetch(OApiEndpointUrl.Products);
+    const url = OApiEndpointUrl.Products;
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch products");
+    const response = await fetch(OApiEndpointUrl.Products).catch((error) => {
+      logError("Failed to fetch products", { url, error });
+    });
+
+    if (!response || !response.ok) {
+      return [] as Product[];
     }
 
-    const data = await response.json();
+    const data = await response.json().catch((err) => {
+      logError("Failed to parse products data from fetch response", {
+        url,
+        err,
+      });
+
+      return [] as Product[];
+    });
 
     return apiToProductsList(data);
   },
