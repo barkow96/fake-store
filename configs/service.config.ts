a4f6d4e568fe7b988a8243daea2372654fe6c700
Bartosz Kowal <bartoszkowal96@gmail.com>
2026-02-10 23:32:53 +0100

    chore: Adjust common request headers

--- configs/service.config.ts
+++ configs/service.config.ts
@@ -1,5 +1,7 @@
 export const serviceConfig = {
   headers: {
     "Content-Type": "application/json",
+    Accept: "application/json",
+    "User-Agent": "FakeStoreApp/1.0 (+https://github.com; server)",
   },
 };
