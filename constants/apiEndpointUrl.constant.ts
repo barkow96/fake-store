66a12eb39c1efe999865776aae151892876727e4
Bartosz Kowal <bartoszkowal96@gmail.com>
2026-02-10 22:58:16 +0100

    chore: Debug purpose logs

--- constants/apiEndpointUrl.constant.ts
+++ constants/apiEndpointUrl.constant.ts
@@ -2,13 +2,6 @@ const DEFAULT_API_URL = "https://fakestoreapi.com";
 
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL ?? DEFAULT_API_URL;
 
-if (typeof window === "undefined" && !process.env.NEXT_PUBLIC_API_URL) {
-  console.warn(
-    "[apiEndpointUrl] NEXT_PUBLIC_API_URL is not set; using",
-    DEFAULT_API_URL,
-  );
-}
-
 export const OApiEndpointUrl = {
   Products: `${API_BASE_URL}/products`,
   Carts: `${API_BASE_URL}/carts`,
